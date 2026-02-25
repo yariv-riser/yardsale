0d5927a4a0dac5c68fceeef0a6ea9ad80084ad46
Yariv Fruend <yarivfrd@gmail.com>
2026-02-25 20:54:58 +0200

    updated nextjs config

--- next.config.js
+++ next.config.js
@@ -12,6 +12,12 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
         port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dgayohewbdi10a3h.private.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
